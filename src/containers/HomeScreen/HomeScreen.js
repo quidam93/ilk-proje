75b723cd431066895471df06e4a9382fbb3f9bdf
Fatih Şen <ftihsen@gmail.com>
2019-06-01 15:21:55 +0300

    - Deneme

--- src/containers/HomeScreen/HomeScreen.js
+++ src/containers/HomeScreen/HomeScreen.js
@@ -1,21 +1,20 @@
-import React,{Component} from 'react';
-import {View,Text,Button} from 'react-native';
-import HomeScreenCarousel from '../../components/HomeScreenCarousel';
+import React, { Component } from "react";
+import { View, Text, Button } from "react-native";
+import HomeScreenCarousel from "../../components/HomeScreenCarousel";
 
-
-class HomeScreen extends Component{
-    render () {
-        
-        return(
-            <View style={{backgroundColor:"red",flex:1}}>
-                <View style={{backgroundColor:"yellow",flex:2}}>
-                    <HomeScreenCarousel></HomeScreenCarousel>
-                </View>
-                <View style={{backgroundColor:"blue",flex:1}}></View>
-                <View style={{backgroundColor:"orange",flex:3}}></View>
-            </View>
-        )
-    }
-    
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ backgroundColor: "red", flex: 1 }}>
+        <View style={{ backgroundColor: "yellow", flex: 2 }}>
+          <HomeScreenCarousel />
+        </View>
+        <View style={{ backgroundColor: "blue", flex: 1 }}>
+          <Text>Denem</Text>
+        </View>
+        <View style={{ backgroundColor: "orange", flex: 3 }} />
+      </View>
+    );
+  }
 }
 export default HomeScreen;
